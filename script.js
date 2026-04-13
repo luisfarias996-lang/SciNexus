9bf469204e18645d3c5026a476bffd96bb5ae96e
luisfarias996-lang <luisfarias996@gmail.com>
2026-04-13 12:03:09 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// let indice = 0
+
+// function mover(direccion){
+//     const track = document.getElementById('track');
+//     const slides = document.getElementById('.slide');
+//     const totalSlides = slides.length;
+// }
+//     indice += direccion;
+
+//     if (indice >= totalSlides) indice = 0;
+//     if (indice < 0) indice = totalSlides - 1;
+
+//     track.style.transform = `translateX(-${indice * 100}%)`;
+    
